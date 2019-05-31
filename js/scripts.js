03bb6bc206213aa4b6955b990493bb6828ee1ce6
bittywahome <bettywahome253@gmail.com>
2019-05-31 18:58:28 +0300

    I have made changed the structure of index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-var trianglelength =parseInt(prompt("enter number1"));
-var trianglewidth= parseInt(prompt("enter  number2"));
-var trianglebase = parseInt(prompt("enter number3"));
+function calculate(){
+ var L=parseFloat(document.getElementById("l").value);
+ var W=parseFloat(document.getElementById("w").value);
+ var B=parseFloat(document.getElementById("b").value);
 
- if(trianglelength == trianglewidth && trianglewidth == trianglebase && trianglebase == trianglelength){
-  alert("Equilateral");
+if((L+W)<=B || (W+B)<=L||(B+L)<=W){
+ document.getElementById('answer').innerHTML="Not a triangle"
+}else if((L == W) && (W == B) && (B == L)){
+  document.getElementById('answer') .innerHTML="Equilateral"
+}else if((L == W)||(W == B)||(B == L)){
+document.getElementById('answer') .innerHTML="isoceles"
+}else if ((L!==W) && (W!==B)&&(B!==L)){
+  document.getElementById('answer').innerHTML="scalene"
 }
-
- else if(trianglelength == trianglewidth||trianglewidth == trianglebase||trianglebase == trianglelength){
-alert("isoceles");
- }
-
-else{
- alert("scalene");
- }
+};
