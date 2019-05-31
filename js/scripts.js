35751eac5477b58c79631d2f089e75ae73a51974
bittywahome <bettywahome253@gmail.com>
2019-05-31 16:47:09 +0300

    i have added scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var trianglelength =parseInt(prompt("enter number1"));
+var trianglewidth= parseInt(prompt("enter  number2"));
+var trianglebase = parseInt(prompt("enter number3"));
+
+ if(trianglelength == trianglewidth && trianglewidth == trianglebase && trianglebase == trianglelength){
+  alert("Equilateral");
+}
+
+ else if(trianglelength == trianglewidth||trianglewidth == trianglebase||trianglebase == trianglelength){
+alert("isoceles");
+ }
+
+else{
+ alert("scalene");
+ }
